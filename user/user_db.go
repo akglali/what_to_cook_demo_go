93395a62f2a16fd86953f3aaa86b74bb63f897cf
akglali <akgol97_@hotmail.com>
2022-12-25 23:24:35 -0500

    for the signup phrase all the required database called has been made.

--- user/user_db.go
+++ user/user_db.go
@@ -1,7 +1,13 @@
 package user
 
-import "what_to_cook_demo_go/database"
+import (
+	"what_to_cook_demo_go/database"
+)
 
+// **** Use Exec whenever we want to insert update or delete
+// **** Doing Exec(query) will not use a prepared statement, so lesser TCP calls to the SQL server
+
+// this code will save the code that has been sent to user's email
 func insertVerify(email, code, now string) error {
 	_, err := database.Db.Exec("insert into verify_users_table(email,code,created_at) values ($1,$2,$3)", email, code, now)
 	if err != nil {
@@ -10,6 +16,7 @@ func insertVerify(email, code, now string) error {
 	return nil
 }
 
+// if user sent the code again it will update the code in the system
 func updateVerify(email, code, now string) error {
 	_, err := database.Db.Exec("update verify_users_table set code=$1 , created_at=$2 where email=$3", code, now, email)
 	if err != nil {
@@ -18,10 +25,43 @@ func updateVerify(email, code, now string) error {
 	return nil
 }
 
+// it inserts the user if user has passed all the required action successfully
 func insertUser(username, firstName, lastName, password, email, token string) error {
 	_, err := database.Db.Exec("insert into users_table(username,firstname,lastname,password,email,token) values ($1,$2,$3,$4,$5,$6)", username, firstName, lastName, password, email, token)
 	if err != nil {
 		return err
 	}
+	// since user is already verified we can delete it from verify user table
+	deleteUserCode(email)
 	return nil
+}
+
+// user code is removed from the database
+func deleteUserCode(email string) {
+	_, err := database.Db.Exec("delete from verify_users_table where email=$1", email)
+	if err != nil {
+		return
+	}
+}
+
+// verify the users code and expiration
+func verifyUser(email string, createdAt *string, code *int) error {
+	err := database.Db.QueryRow("select created_at,code from verify_users_table where email=$1", email).Scan(&*createdAt, &*code)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// if user is already exist we don't send a code
+func userExist(email string) (error, bool) {
+	var isUserExist bool
+	err := database.Db.QueryRow("select exists(select 1 from users_table where email=$1)", email).Scan(&isUserExist)
+	if err != nil {
+		return err, false
+	}
+	if isUserExist == true {
+		return nil, true
+	}
+	return nil, false
 }
