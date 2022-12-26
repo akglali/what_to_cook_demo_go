b4e1b47bd1d04ae894eef973cee39bde43b49d6c
akglali <akgol97_@hotmail.com>
2022-12-25 23:25:41 -0500

    time diff is calculating.

--- user/user_helper.go
+++ user/user_helper.go
@@ -10,17 +10,23 @@ import (
 	"time"
 )
 
+const dateFormat = "2006-01-02 15:04:05"
+const sqlDateFormat = "2006-01-02T15:04:05Z"
+
+// hash the password for users safety
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 
 }
 
+// it checks the hashed password
 func checkPassword(pass, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
 	return err == nil
 }
 
+// it generates the user token
 func tokenGenerator() string {
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
@@ -34,9 +40,10 @@ func tokenGenerator() string {
 	return hex.EncodeToString(b)
 }
 
-const otpChars = "123456789"
+// i use it to create a random code
+const otpChars = "1234567890"
 
-// 6 digits code is generated!
+// GenerateOTP 6 digits code is generated!
 func GenerateOTP(length int) (string, error) {
 	buffer := make([]byte, length)
 	_, err := rand.Read(buffer)
@@ -69,4 +76,22 @@ func sendEmail(subject, body, email string) error {
 		body + "\r\n")
 	err := smtp.SendMail(fmt.Sprintf("%s:%d", host, port), auth, from, to, msg)
 	return err
+}
+
+// check if the code is expired
+func timeDiffSec(createdAt string) bool {
+	currentTime := time.Now()
+
+	// we need to convert the time location. To make the right calculation
+	loc := currentTime.Location()
+	date, err := time.ParseInLocation(sqlDateFormat, createdAt, loc)
+	if err != nil {
+		return false
+	}
+	diff := currentTime.Sub(date).Seconds()
+	if diff > 300 {
+		return false
+	}
+	return true
+
 }
