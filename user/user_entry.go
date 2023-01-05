540b2e6543211542337040793c69df5963e302c3
akglali <akgol97_@hotmail.com>
2023-01-04 20:53:32 -0500

    some code modification has been made.

--- user/user_entry.go
+++ user/user_entry.go
@@ -12,28 +12,23 @@ import (
 func SetupEntry(rg *gin.RouterGroup) {
 	rg.POST("", SignUp)
 	rg.POST("/Verify", verifyAccount)
-	rg.GET("/something", something)
 }
 
 func SignUp(c *gin.Context) {
 	body := SignUpDTO{}
-	helpers.AcceptMethod(&body, c)
-	// it will be checked in front end too.
-	if helpers.ValidateLen(body.Username, 6) || helpers.ValidateLen(body.Password, 6) {
-		helpers.NotAcceptableAbort(c, "Password and username must be more than 6 character")
+	passedMarshall := helpers.AcceptMethod(&body, c)
+	if !passedMarshall {
 		return
 	}
 	var createdAt string
-	var code int
-	err := verifyUser(body.Email, &createdAt, &code)
-	if err != nil {
+	var code string
+	if err := verifyUser(body.Email, &createdAt, &code); err != nil {
 		fmt.Println(err)
 		helpers.NotAcceptableAbort(c, "Something went wrong!")
 		return
 	}
 	//check if user enters the code within 121 seconds
-	isExpired := timeDiffSec(createdAt)
-	if isExpired == false {
+	if isExpired := timeDiffSec(createdAt); isExpired == false {
 		helpers.NotAcceptableAbort(c, "Time is expired send another code!")
 		return
 	}
@@ -45,8 +40,7 @@ func SignUp(c *gin.Context) {
 
 	password, _ := hashPassword(body.Password)
 	token := tokenGenerator()
-	err = insertUser(body.Username, password, body.Email, token)
-	if err != nil {
+	if err := insertUser(body.Username, password, body.Email, token); err != nil {
 		fmt.Println(err)
 		helpers.BadRequestAbort(c, "User is already exist")
 		return
@@ -61,32 +55,36 @@ func SignUp(c *gin.Context) {
 }
 
 func verifyAccount(c *gin.Context) {
-	body := VerifyUserSt{}
-	helpers.AcceptMethod(&body, c)
+	body := VerifyUserDTO{}
+	passedMarshall := helpers.AcceptMethod(&body, c)
+	if !passedMarshall {
+		return
+	}
 	//checking if user is already exist before sent the email.
 	err, isUserExist := userExist(body.Email)
 	if err != nil || isUserExist {
-		fmt.Println(err)
 		helpers.BadRequestAbort(c, "User is already exist")
 		return
 	}
 	code, _ := GenerateOTP(6)
 	email := body.Email
-	/*	subject := "Test Email"
-		emailBody := "Your code is " + code*/
-	fmt.Println(code)
+	subject := "Test Email"
+	emailBody := "Your code is " + code
 	now := time.Now().Format(dateFormat)
-	err = insertVerify(email, code, now)
-	if err != nil {
+	if err := insertVerify(email, code, now); err != nil {
 		// if error is because of duplication we update the database. 23505 is duplicate error code
 		if string(err.(*pq.Error).Code) == "23505" {
 			fmt.Println("user hasn't verified yet")
-			err = updateVerify(email, code, now)
-			/*	err = sendEmail(subject, emailBody, email)
-				if err != nil {
-					helpers.BadRequestAbort(c, "Code is not sent please try again")
-					return
-				}*/
+			if err := updateVerify(email, code, now); err != nil {
+				// if database can't update the code.
+				helpers.BadRequestAbort(c, "Something went wrong")
+				return
+			}
+			//send email to the same address since it is already created.
+			if err := sendEmail(subject, emailBody, email); err != nil {
+				helpers.BadRequestAbort(c, "Code is not sent please try again")
+				return
+			}
 			c.JSON(200, "Your code is sent to "+body.Email)
 			return
 		} else {
@@ -95,23 +93,10 @@ func verifyAccount(c *gin.Context) {
 		return
 	}
 	//that sends the email
-	/*err = sendEmail(subject, emailBody, email)
-	if err != nil {
+	if err := sendEmail(subject, emailBody, email); err != nil {
 		helpers.BadRequestAbort(c, "Code is not sent please try again")
 		return
-	}*/
+	}
 	c.JSON(200, "Your code is sent to "+body.Email)
-
-}
-
-func logout(c *gin.Context) {
-	token := c.GetHeader("token")
-	fmt.Println(token)
-}
-
-func something(c *gin.Context) {
-	token := c.GetHeader("Token")
-	verifyJwt := helpers.VerifyJWT(token, c)
-	fmt.Println(verifyJwt)
 
 }
