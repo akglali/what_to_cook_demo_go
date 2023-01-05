ec457f0fac0fa4141788162d843fd300107cb11e
akglali <akgol97_@hotmail.com>
2023-01-04 20:49:31 -0500

    Validation has been added and code is changed to string.

--- user/user_struct.go
+++ user/user_struct.go
@@ -1,12 +1,12 @@
 package user
 
 type SignUpDTO struct {
-	Username string `db:"username"`
-	Password string `db:"password"`
-	Email    string `db:"email"`
-	Code     int    `db:"code"`
+	Username string `db:"username" form:"username" json:"username" validate:"required,min=6,max=18"`
+	Password string `db:"password" form:"password" json:"password" validate:"required,min=6,max=18"`
+	Email    string `db:"email" form:"email" json:"email" validate:"required"`
+	Code     string `db:"code" form:"code" json:"code" validate:"required"`
 }
 
-type VerifyUserSt struct {
-	Email string `db:"email"`
+type VerifyUserDTO struct {
+	Email string `db:"email" form:"email" json:"email" validate:"required"`
 }
